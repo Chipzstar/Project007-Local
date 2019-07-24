ece1b3f202b5f2b7986f9f74be90c565de495d93
Chisom Oguibe <chisom.oguibe@spicymango.co.uk>
2019-07-24 15:57:00 +0100

    Styling of Models Screen

--- app/screens/ModelsScreen/ModelsScreen.js
+++ app/screens/ModelsScreen/ModelsScreen.js
@@ -1,15 +1,20 @@
 import React, {Component} from 'react';
-import {View, Text } from 'react-native';
+import {Image} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
-import {Left, Button, Header, Content, Body, Title, Footer } from 'native-base';
+import {Left, Button, Header, Content, Body, Footer } from 'native-base';
 import {withNavigation} from "react-navigation";
 import {HideNavigationBar} from "react-native-navigation-bar-color";
 import * as firebase from 'react-native-firebase';
-//icons
+//images & icons
 import AMIcon from '../../assets/icons/icon_font';
+import headerTitle from '../../assets/images/drawable-xxxhdpi/models_header.png';
 
 //components
+import VantageTile from "../../components/AsyncImages/VantageTile";
+import DB11Tile from "../../components/AsyncImages/DB11Tile";
+import DBSTile from "../../components/AsyncImages/DBSTile";
 import NavWheel from '../../components/NavWheel';
+
 //styles
 import styles from './styles';
 
@@ -39,13 +44,13 @@ class ModelsScreen extends Component {
                         </Button>
                     </Left>
                     <Body>
-                        <Title>Models</Title>
+                        <Image source={headerTitle} resizeMode="contain" style={styles.header}/>
                     </Body>
                 </Header>
                 <Content contentContainerStyle={styles.contentContainer}>
-                    <View style={styles.welcome}>
-                        <Text style={styles.text}>Welcome to the ModelsScreen Screen!</Text>
-                    </View>
+                    <VantageTile/>
+                    <DB11Tile/>
+                    <DBSTile/>
                 </Content>
                 <NavWheel/>
                 <Footer style={{backgroundColor: 'transparent'}}>
